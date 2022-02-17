edfa6b3d7298fd1685e5e6e9f07f834daa26ff9f
Omkarbade <omakrbade3799@gmail.com>
2022-02-18 01:37:03 +0530

    [Omkar] Add Banking cash counter program

---
+++ BankingCashCounter/CashCounter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingCashCounter
+{
+    public class CashCounter
+    {
+        public int Total_Balance_Amount = 100000;
+        public int deposit_Amt = 0;
+        public int withdraw_Amt = 0;
+
+        Queue queue = new Queue();
+
+        public void Withdraw()
+        {
+            Console.WriteLine("Total Balance of your account : " +Total_Balance_Amount);
+
+            Console.WriteLine("Please Enter Amount for Withdraw :\n");
+            withdraw_Amt = Convert.ToInt32(Console.ReadLine());
+
+            if (withdraw_Amt > Total_Balance_Amount)  //if else loop used for withdrew in that 2 condition;
+            {
+                Console.WriteLine("Insufficient balance in your account..");
+            }
+            else
+            {
+                Total_Balance_Amount -= withdraw_Amt;
+                Console.WriteLine(withdraw_Amt + " amount withdrawn from your account \n");
+                Console.WriteLine("Your Avaialble Balance is :" +Total_Balance_Amount);
+            }
+            queue.Dequeue();
+        }
+
+        public void Deposit()
+        {
+            Console.WriteLine("Total Balance of your account : " +Total_Balance_Amount);
+            Console.WriteLine("Please Enter Amount for Deposit :\n");
+            deposit_Amt = Convert.ToInt32(Console.ReadLine());
+
+            Total_Balance_Amount += deposit_Amt;
+            Console.WriteLine(deposit_Amt + " Money depositted in your account \n");
+            Console.WriteLine("Now Your Avaialble Balance is :" +Total_Balance_Amount);
+
+            queue.Dequeue();
+
+        }
+    }
+}
---
+++ BankingCashCounter/Quere.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankingCashCounter
+{
+    public class Queue
+    {
+        Node head = null;
+
+        // method used enqueue
+        public void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+            }
+            Console.WriteLine(node.data);
+        }
+
+        // method display
+        public void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        //method dequeue
+        public void Dequeue()
+        {
+            if (this.head == null)
+            {
+                return;
+            }
+            else
+            {
+                while (this.head != null)
+                {
+                    head.next.next = null;
+                }
+                head.next = head;
+            }
+        }
+    }
+}
